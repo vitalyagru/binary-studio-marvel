f4e60759823d80027012d24209b77985c5344b3d
vitalyagru <vitalya.diesel@mail.ru>
2016-05-24 14:40:26 +0300

    added composer

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+  require __DIR__ . '/../vendor/autoload.php';
+
+  namespace Marvel\Atlantida;
+  class NamorSubMaries {
+    public static function whoami() {
+
+    }
+  }
+  echo NamorSubMaries::whoami(); //I'm NamorSubMariner from Atlantida
