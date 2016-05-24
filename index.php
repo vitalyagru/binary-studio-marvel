eb9b971517c31009999e8bce4a909ae9683c7a61
vitalyagru <vitalya.diesel@mail.ru>
2016-05-24 17:48:58 +0300

    added trait

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
-  require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-  echo Marvel\NamorSubMaries::whoami();
+echo Marvel\NamorSubMaries::whoami();
   // $test = new Hello\Test;
   // $test->sayHello();
+trait RandomQuote {
+  public function getQuote(){
+    return;
+  }
+}
+
+class Greeting {
+
+  use RandomQuote;
+
+  public function say($name) {
+    echo 'Hi, ' . $name . '! There is a new quote for you: ' . $this->getQuote();
+  }
+}
