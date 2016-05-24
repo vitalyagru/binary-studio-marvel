d48dfc3dd77b9f3215edf6c990c04302584229cd
vitalyagru <vitalya.diesel@mail.ru>
2016-05-24 16:25:20 +0300

    added class NamorSubMaries

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-  require __DIR__ . '/../vendor/autoload.php';
+  require __DIR__ . '/vendor/autoload.php';
 
-  namespace Marvel\Atlantida;
-  class NamorSubMaries {
-    public static function whoami() {
-
-    }
-  }
-  echo NamorSubMaries::whoami(); //I'm NamorSubMariner from Atlantida
+  echo Marvel\NamorSubMaries::whoami();
+  // $test = new Hello\Test;
+  // $test->sayHello();
---
+++ src/NamorSubMaries.php
@@ -0,0 +1,11 @@
+<?php
+   //namespace Marvel\Atlantida;
+  namespace Marvel;
+
+  class NamorSubMaries {
+    public static function whoami() {
+      echo "I'm NamorSubMariner from Atlantida";
+    }
+  }
+
+
