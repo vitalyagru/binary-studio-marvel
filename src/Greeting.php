232fe7f07f764719369009c440aef6a0c6dd2bfe
vitalyagru <vitalya.diesel@mail.ru>
2016-05-25 03:07:03 +0300

    adde Application class

---
+++ src/Greeting.php
@@ -0,0 +1,26 @@
+<?php
+namespace Marvel;
+trait RandomQuote {
+private $_quotes = ['A dream becomes a goal when action is taken toward its achievement.',
+            'It does not matter how slowly you go so long as you do not stop.',
+            'Everything takes longer than you think.',
+            'Fortune is easily found, but hard to be kept.',
+            'The actions of men are the best interpreters of their thoughts.',
+            'Money speaks sense in a language all nations understand.',
+            'To the world you may be just one person, but to one person you may be the world.',
+            'Experience is the name every one gives to their mistakes.',
+            'Success is not in what you have, but who you are.',
+            'Illusion is the first of all pleasures.'];
+  public function getQuote(){
+    return $this->_quotes[0];
+  }
+}
+
+class Greeting {
+
+  use RandomQuote;
+
+  public function say($name = 'Guest') {
+    echo 'Hi, ' . $name . '! There is a new quote for you: &#10077;' . $this->getQuote() . '&#10078;';
+  }
+}
