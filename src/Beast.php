10c3f37e49b7a5803a3a4cd2647cebb27166ce54
vitalyagru <vitalya.diesel@mail.ru>
2016-05-25 03:10:01 +0300

    added personages

---
+++ src/Beast.php
@@ -0,0 +1,11 @@
+<?php
+   //namespace Marvel\Atlantida;
+  namespace Marvel;
+
+  class Beast {
+    public static function whoami() {
+      echo "I'm NamorSubMariner from Atlantida";
+    }
+  }
+
+
